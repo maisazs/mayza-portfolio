4763f353f72557c1a858938addff31b22de3b542
Mayza Ester <contatomayza@hotmail.com>
2026-07-02 00:00:06 -0300

    Remove arquivos de build do versionamento

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jHOAka8IjLTEzqi6uI4RJVKqwD6eewvaNnAzbNHOlzs=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_0zr2k9a._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0p3_c3i._.js","static/chunks/node_modules_1am8cpo._.js","static/chunks/components_sections_Hero_module_084bq-o.css"],
-    source: "dynamic"
-});
